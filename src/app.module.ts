8291082319b33d292f977e23a0d80adfb4b3b47e
nquduy65 <nguyenquocduy06051998@gmail.com>
2024-04-05 17:19:19 +0700

    update authentication

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
